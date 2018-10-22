bff658205c024ed23578636462ff00486cad274e
Ashlesh Khajbage <ashleshuk@gmail.com>
2018-10-22 22:18:22 +0530

    GET /todos route added

--- server/server.js
+++ server/server.js
@@ -1,4 +1,4 @@
-//library imports
+ //library imports
 var express =require('express');
 var bodyParser= require('body-parser');
 
@@ -29,7 +29,15 @@ app.post('/todos',(req,res)=>{
     (e)=>{
         res.status(400).send(e);
 
-    })
+    });
+});
+
+app.get('/todos',(req,res)=>{
+    Todo.find().then((todos)=>{
+          res.send({todos});
+    },(e)=>{
+        res.status(400).send(e);
+    });
 });
 
 
@@ -39,7 +47,7 @@ app.listen(3000,()=>{
     
 });
 
-
+module.exports = {app};
 //created a new todo
 // var newTodo = new Todo({
 //     text:'Cook dinner'
